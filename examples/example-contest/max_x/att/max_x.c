d8e9ad45d058040699cf657b1ccf445d16d5122a
djsdvg <leonardo.ballini@protonmail.com>
2021-03-01 23:05:37 -0800

    Adattato max_x per girare su TA

--- examples/example-contest/max_x/att/max_x.c
+++ examples/example-contest/max_x/att/max_x.c
@@ -1,5 +1,6 @@
 // problem: max_x, example of a solution file, Romeo Rizzi Jan 2015
-#include "ourLibToPlay.h"
+int pesa(long int bigliaA, long int bigliaB);
+void pensoCheMaxSia(long int risp);
 void individua(long int n, long int maxLies) {
   if( pesa(0,n-1) > 0)  pensoCheMaxSia(0); 
   else pensoCheMaxSia(n-1); 
